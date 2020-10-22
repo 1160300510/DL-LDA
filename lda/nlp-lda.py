9884d7341c74710431c58029e30258670fd2ecc3
1160300510 <379744795@qq.com>
2020-10-22 22:22:20 +0800

    lda

---
+++ lda/nlp-lda.py
@@ -0,0 +1,152 @@
+import pandas as pd
+import preprocess.pre as pre
+from nltk.tokenize import RegexpTokenizer
+from nltk.stem.wordnet import WordNetLemmatizer
+from gensim.models import Phrases
+# Remove rare and common tokens.
+from gensim.corpora import Dictionary
+# Train LDA model.
+from gensim.models import LdaModel
+from pprint import pprint
+import numpy as np
+import logging
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+
+if __name__ == '__main__':
+
+    best_coherence = -100
+    best_num_topics = 0
+    dictionary = Dictionary.load('../data/nlp.dict')
+
+    # Bag-of-words representation of the documents.
+    # corpus = [dictionary.doc2bow(doc) for doc in docs]
+    # np.save('../data/nlp_corpus.npy',np.array(corpus))
+    corpus = np.load('../data/nlp_corpus.npy', allow_pickle=True).tolist()
+    print('Number of unique tokens: %d' % len(dictionary))
+    print('Number of documents: %d' % len(corpus))
+    chunksize = 2000
+    passes = 30
+    iterations = 400
+    eval_every = None  # Don't evaluate model perplexity, takes too much time.
+
+    # # Make a index to word dictionary.
+    temp = dictionary[0]  # This is only to "load" the dictionary.
+    id2word = dictionary.id2token
+
+    for i in range(5,25):
+
+        num_topics = i
+        model = LdaModel(
+            corpus=corpus,
+            id2word=id2word,
+            chunksize=chunksize,
+            alpha='auto',
+            eta='auto',
+            iterations=iterations,
+            num_topics=num_topics,
+            passes=passes,
+            eval_every=eval_every
+        )
+
+        top_topics = model.top_topics(corpus)  # , num_words=20)
+
+        # Average topic coherence is the sum of topic coherences of all topics, divided by the number of topics.
+        avg_topic_coherence = sum([t[1] for t in top_topics]) / num_topics
+        if avg_topic_coherence > best_coherence:
+            best_num_topics = i
+            best_coherence = avg_topic_coherence
+        print('Average topic coherence: %.4f.' % avg_topic_coherence)
+        pprint(top_topics)
+        # model.save('../model/nlp_10/nlp_10.model')
+        model.print_topics(num_topics=i, num_words=15)
+
+    print("best coherence: "+best_coherence)
+    print("best topic nums: "+best_num_topics)
+
+    # df = pd.read_csv('../preprocess/nlp-process.csv')
+    # docs = []
+    # for index, row in df.iterrows():
+    #     body = row['Body']
+    #     docs.append(body)
+    # print(len(docs))
+    # print(docs[0][:50])
+    #
+    # # Split the documents into tokens.
+    # tokenizer = RegexpTokenizer(r'\w+')
+    # for idx in range(len(docs)):
+    #     docs[idx] = tokenizer.tokenize(docs[idx])  # Split into words.
+    #
+    # # Remove numbers, but not words that contain numbers.
+    # docs = [[token for token in doc if not token.isnumeric()] for doc in docs]
+    #
+    # # Remove words that are only one character.
+    # docs = [[token for token in doc if len(token) > 1] for doc in docs]
+    #
+    # # lemmatizer = WordNetLemmatizer()
+    # # docs = [[lemmatizer.lemmatize(token) for token in doc] for doc in docs]
+    # #
+    # # # Add bigrams and trigrams to docs (only ones that appear 20 times or more).
+    # # bigram = Phrases(docs, min_count=20)
+    # # for idx in range(len(docs)):
+    # #     for token in bigram[docs[idx]]:
+    # #         if '_' in token:
+    # #             # Token is a bigram, add to document.
+    # #             docs[idx].append(token)
+    #
+    # # Create a dictionary representation of the documents.
+    # dictionary = Dictionary(docs)
+    # dictionary.save('../data/nlp.dict')
+    # # dictionary = Dictionary.load('../data/nlp.dict')
+    #
+    # # Bag-of-words representation of the documents.
+    # corpus = [dictionary.doc2bow(doc) for doc in docs]
+    # np.save('../data/nlp_corpus.npy',np.array(corpus))
+    # # corpus = np.load('../data/nlp_corpus.npy').tolist()
+    #
+    #
+    # print('Number of unique tokens: %d' % len(dictionary))
+    # print('Number of documents: %d' % len(corpus))
+    #
+    # # for i in range(10,30):
+    # #     trainmodel(i, corpus, dictionary.id2token)
+    #
+    # num_topics = 10
+    # chunksize = 2000
+    # passes = 30
+    # iterations = 400
+    # eval_every = None  # Don't evaluate model perplexity, takes too much time.
+    #
+    # # # Make a index to word dictionary.
+    # temp = dictionary[0]  # This is only to "load" the dictionary.
+    # id2word = dictionary.id2token
+    #
+    # model = LdaModel(
+    #     corpus=corpus,
+    #     id2word=id2word,
+    #     chunksize=chunksize,
+    #     alpha='auto',
+    #     eta='auto',
+    #     iterations=iterations,
+    #     num_topics=num_topics,
+    #     passes=passes,
+    #     eval_every=eval_every
+    # )
+    #
+    # top_topics = model.top_topics(corpus)  # , num_words=20)
+    #
+    # # Average topic coherence is the sum of topic coherences of all topics, divided by the number of topics.
+    # avg_topic_coherence = sum([t[1] for t in top_topics]) / num_topics
+    # print('Average topic coherence: %.4f.' % avg_topic_coherence)
+    # pprint(top_topics)
+    # model.save('../model/nlp_10/nlp_10.model')
+    # model.print_topics(10,15)
+
+
+
+
+
+
+
+
+
---
+++ preprocess/pre.py
@@ -0,0 +1,133 @@
+import re
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem.snowball import SnowballStemmer
+import pandas as pd
+from nltk.stem.wordnet import WordNetLemmatizer
+
+#去除text中的所有非字母内容，包括标点符号、空格、换行、下划线等
+def replace_all_blank(text):
+    text = re.sub("[^a-zA-Z]", " ", text)
+    res = " ".join(text.split())  # 删除多余的空格
+    return res
+
+#判断是否为数字
+def isNumber(word):
+    matchobj = re.match(r"\d+", word, re.M|re.I)
+    if matchobj:
+        return True
+    else:
+        return False
+
+#过滤代码片段
+def codefilter(htmlstr):
+    re_code = re.compile('<code>\w+</code>')
+    s = re_code.sub('', htmlstr)
+    return s
+
+#过滤html中的标签
+def htmlfilter(htmlstr):
+    # 先过滤CDATA
+    re_cdata = re.compile('//<!\[CDATA\[[^>]*//\]\]>', re.I)  # 匹配CDATA
+    re_script = re.compile('<\s*script[^>]*>[^<]*<\s*/\s*script\s*>', re.I)  # Script
+    re_style = re.compile('<\s*style[^>]*>[^<]*<\s*/\s*style\s*>', re.I)  # style
+    re_br = re.compile('<br\s*?/?>')  # 处理换行
+    re_h = re.compile('</?\w+[^>]*>')  # HTML标签
+    re_comment = re.compile('<!--[^>]*-->')  # HTML注释
+    s = re_cdata.sub('', htmlstr)  # 去掉CDATA
+    s = re_script.sub('', s)  # 去掉SCRIPT
+    s = re_style.sub('', s)  # 去掉style
+    # s = re_br.sub('\n', s)  # 将br转换为换行
+    s = re_br.sub(' ', s)
+    s = re_h.sub('', s)  # 去掉HTML 标签
+    s = re_comment.sub('', s)  # 去掉HTML注释
+    # 去掉多余的空行
+    blank_line = re.compile('\n+')
+    s = blank_line.sub(' ', s)
+    return s
+##替换常用HTML字符实体.
+# 使用正常的字符替换HTML中特殊的字符实体.
+# 你可以添加新的实体字符到CHAR_ENTITIES中,处理更多HTML字符实体.
+# @param htmlstr HTML字符串.
+def replaceCharEntity(htmlstr):
+    CHAR_ENTITIES = {'nbsp': ' ', '160': ' ',
+                     'lt': '<', '60': '<',
+                     'gt': '>', '62': '>',
+                     'amp': '&', '38': '&',
+                     'quot': '"', '34': '"', }
+
+    re_charEntity = re.compile(r'&#?(?P<name>\w+);')
+    sz = re_charEntity.search(htmlstr)
+    while sz:
+        entity = sz.group()  # entity全称，如&gt;
+        key = sz.group('name')  # 去除&;后entity,如&gt;为gt
+        try:
+            htmlstr = re_charEntity.sub(CHAR_ENTITIES[key], htmlstr, 1)
+            sz = re_charEntity.search(htmlstr)
+        except KeyError:
+            # 以空串代替
+            htmlstr = re_charEntity.sub('', htmlstr, 1)
+            sz = re_charEntity.search(htmlstr)
+    return htmlstr
+
+#去除停用词,小写,词干化
+def preprocess(str):
+    #小写
+    str = str.lower()
+    #去除数字，标点符号和非字母符号
+    str = replace_all_blank(str)
+    #分词
+    words = word_tokenize(str)
+    #词形还原和词干处理
+    lemma = WordNetLemmatizer()
+    stem = SnowballStemmer("english")
+    wordlemmas = []
+    for word in words:
+        word = lemma.lemmatize(word)
+        # word = stem.stem(word)
+        wordlemmas.append(word)
+    #英语停用词
+    english_stopwords = stopwords.words("english")
+    words_stopwordsremoved = []
+    for word in wordlemmas:
+        if word not in english_stopwords:
+            words_stopwordsremoved.append(word)
+    result = ''
+    for word in words_stopwordsremoved:
+        result += ' ' + word
+    return result.strip()
+#处理post的body部分
+def processbody(str):
+    str = replaceCharEntity(str)
+    str = codefilter(str)
+    str = htmlfilter(str)
+    str = preprocess(str)
+    return str
+
+#处理post的tag部分
+def preprocesstag(str):
+    p1 = re.compile(r'[<](.*?)[>]', re.S)
+    results = re.findall(p1, str)
+    tag = ''
+    for s in results:
+        tag += s + ' '
+    tag = tag[:-1]
+    return tag
+
+if __name__ == '__main__':
+    df = pd.read_csv('../data/nlp.csv')
+    for index, row in df.iterrows():
+        title = str(row['Title'])
+        tags = str(row['Tags'])
+        body = str(row['Body'])
+        title = processbody(title)
+        print(title)
+        body = processbody(body)
+        tags = preprocesstag(tags)
+        row['Title'] = title
+        print(row['Title'])
+        row['Tags'] = tags
+        row['Body'] = body
+        # if(len(body)<20 or len(body)>1000):
+        #     df.drop(index=index, inplace=True, axis=0)
+    df.to_csv('../preprocess/nlp-process.csv')
